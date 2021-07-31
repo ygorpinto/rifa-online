2f8e922514acb67ec75cba8c0ce29726daca0155
ygorpinto <ygor.pinto@hotmail.com>
2021-07-31 20:22:20 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 
+
 function App() {
+
+  interface Rifa {
+    owner?:string,
+    number?:number
+  };
+
+  const bringAllData = () => {
+    // buscar dados no db
+  }
+
   return (
     <div>
     </div>
