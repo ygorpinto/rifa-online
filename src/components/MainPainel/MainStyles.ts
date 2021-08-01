ad0d1e79a9a0ab7ab607fd0c791a1aaa84cc654f
ygorpinto <ygor.pinto@hotmail.com>
2021-08-01 10:18:18 -0300

    Ok

--- src/components/MainPainel/MainStyles.ts
+++ src/components/MainPainel/MainStyles.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components'
 
 const MainStyles = styled.div`
+
 height:80vh;
 width:100%;
 
@@ -20,15 +21,26 @@ flex-direction:column;
 }
 
 
+.modal {
 
+    height:100vh;
+    width:100%;
+
+    z-index: 1000;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+}
 
 button {
+
     background-color:#fff;
     border-radius:6px;
     border:2px 2px solid rgba(0,0,0,0.3);
     margin:1rem;
 
     &:hover {
+
         background-color:#bb94c0;
         color:#fff;
     }
