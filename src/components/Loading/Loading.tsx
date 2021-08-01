de53112d29d6ad36e0edcbf6ff2447edbc70824f
ygorpinto <ygor.pinto@hotmail.com>
2021-08-01 12:43:01 -0300

    ok

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoadingStyles from './LoadingStyles';
+
+function Loading () {
+    return (
+        <LoadingStyles>
+            <img
+            src="marie2.gif"/>
+        </LoadingStyles>
+    )
+}
+
+export default Loading;
---
+++ src/components/Loading/LoadingStyles.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const LoadingStyles = styled.div`
+height:100vh;
+width:100%;
+`
+
+export default LoadingStyles;
