51710c24c7de86e3ce06737b4a7ec380a9cbab97
ygorpinto <ygor.pinto@hotmail.com>
2021-07-31 21:16:47 -0300

    ok

--- src/components/MainPainel/MainStyles.ts
+++ src/components/MainPainel/MainStyles.ts
@@ -5,18 +5,33 @@ const MainStyles = styled.div`
 height:80vh;
 width:100%;
 
-.rifasContainer {
-
-}
-
 display:flex;
 align-items:center;
 flex-direction:column;
 
-flex-wrap: wrap;
+.rifasContainer {
+
+    margin-top: 4rem;
+    
+    height:80vh;
+    width:69%;
+    
+    flex-wrap: wrap;
+}
+
+
+
 
 button {
+    background-color:#fff;
+    border-radius:6px;
+    border:2px 2px solid rgba(0,0,0,0.3);
     margin:1rem;
+
+    &:hover {
+        background-color:#bb94c0;
+        color:#fff;
+    }
 }
 
 `
